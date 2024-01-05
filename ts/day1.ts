e7e4d6161cb4cb3efdcc760dbedf3dea63595cc8
bradleygichuru <bradleygichuru@gmail.com>
2024-01-05 21:53:35 +0300

    initial commit

---
+++ ts/day1.ts
@@ -0,0 +1,29 @@
+import { createReadStream } from 'fs';
+const readline =require("readline");
+const fileStream = createReadStream('example.txt');
+const rl = readline.createInterface({
+    input: fileStream,
+    crlfDelay: Infinity
+  });
+let sum = 0;
+rl.on('line', function (line:string) {
+	const numbers:number[] = []
+	let lineNum = 0;
+	[...line].forEach(char => {
+		if(Number.isInteger(parseInt(char))){
+			numbers.push(parseInt(char))
+			//console.log(char)
+		}
+	}); if (numbers.length > 1) {
+		
+		sum = sum+ parseInt(`${numbers[0]}${numbers[numbers.length - 1]}`);
+		lineNum = parseInt(`${numbers[0]}${numbers[numbers.length - 1]}`)
+		
+	}else{
+		sum = sum + parseInt(`${numbers[0]}${numbers[0]}`) ;
+		lineNum = parseInt(`${numbers[0]}${numbers[0]}`);
+	}
+
+console.log(`Sum: ${sum}`)
+  console.log(`Line from file: ${line} calib: ${lineNum}`);
+});
